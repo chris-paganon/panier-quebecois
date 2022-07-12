0f75fb1e8a6d708e977a06f62a6401cba54ce8b1
PixelJC <cpaganon@gmail.com>
2022-07-12 15:32:16 -0400

    Add the Atwater filter

--- wp-content/plugins/00-panier-quebecois/includes/ajax-shop/pq-ajax-shop-starter.php
+++ wp-content/plugins/00-panier-quebecois/includes/ajax-shop/pq-ajax-shop-starter.php
@@ -359,6 +359,15 @@ function prod_filters_list() {
           <li class="lesProducts menu-item menu-item-type-custom menu-item-object-custom">
             <label for="prod_filter-marche">
               <?php $filtersArray = (isset($_GET['filters'])) ? explode(',', $_GET['filters']) : array(); ?>
+              <input type="checkbox" class='prod_filter' name="prod_filter-marche" value="marche-atwater" <?= (in_array('marche-atwater', $filtersArray)) ? 'checked' : '' ?>>
+              <span class="customcheckbox"></span> <span class="checkedlabel">Marché Atwater</span> 
+            </label>
+            <br>
+          </li>
+
+          <li class="lesProducts menu-item menu-item-type-custom menu-item-object-custom">
+            <label for="prod_filter-marche">
+              <?php $filtersArray = (isset($_GET['filters'])) ? explode(',', $_GET['filters']) : array(); ?>
               <input type="checkbox" class='prod_filter' name="prod_filter-marche" value="vegan" <?= (in_array('vegan', $filtersArray)) ? 'checked' : '' ?>>
               <span class="customcheckbox"></span> <span class="checkedlabel">Végé/Vegan</span> 
             </label>
