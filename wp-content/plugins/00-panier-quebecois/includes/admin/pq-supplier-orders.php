f6468bf5bcbf0641ad8c05024fb4da7d7fe2cce0
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-08-01 15:02:36 -0400

    Reorganize supplier orders to accomodate emails

--- wp-content/plugins/00-panier-quebecois/includes/admin/pq-supplier-orders.php
+++ wp-content/plugins/00-panier-quebecois/includes/admin/pq-supplier-orders.php
@@ -16,74 +16,94 @@ function pq_supplier_order_fct() {
         $delivery_date_raw = pq_get_current_delivery_date_for_supplier();
         $orders = myfct_get_relevant_orders( $delivery_date_raw, "" );
 
-        $supplier_id = 676;
-        $supplier = get_term_by( 'id', $supplier_id, 'pq_producer' );
+        $supplier_id = 195;
+        $supplier = get_term_by( 'id', $supplier_id, 'product_tag' );
 
-        $products = array();
-        foreach ( $orders as $order ) {
-            foreach ( $order->get_items() as $item_id => $item ) {
-                $product = wc_get_product( $item->get_product_id() );
+        $products = pq_get_products_array_for_supplier( $supplier, $orders );
+        $supplier_order_html = pq_get_supplier_order_table( $products );
+
+        return $supplier_order_html;
+    }
+}
+
+
+/**
+ * Get supplier order table
+ */
+function pq_get_supplier_order_table( $products ) {
+
+    ob_start();
+
+    ?>
+    <table>
+        <tr>
+            <th>Produit</th>
+            <th>Quantité</th>
+            <th>Poids</th>
+        </tr>
+
+    <?php
+
+    foreach ( $products as $product_id => $quantity ) {
+        $product = wc_get_product( $product_id );
+        $short_name = get_post_meta( $product_id, '_short_name', true);
+        $weight = get_post_meta( $product_id, '_pq_weight', true );
+        $unit = get_post_meta( $product_id, '_lot_unit', true );
+        $weight_with_unit = $weight . $unit;
+        ?>
+        <tr>
+            <td><?php echo $short_name; ?></td>
+            <td><?php echo $quantity; ?></td>
+            <td><?php echo $weight_with_unit; ?></td>
+        </tr>
+        <?php
+    }
+    ?>
+    </table>
+    <?php
+
+    return ob_get_clean();
+}
+
+
+/**
+ * Get array of products total quantity for a supplier
+ */
+function pq_get_products_array_for_supplier( $supplier, $orders ) {
+    $products = array();
+    foreach ( $orders as $order ) {
+        foreach ( $order->get_items() as $item_id => $item ) {
+            $product = wc_get_product( $item->get_product_id() );
+    
+            if ( pq_is_relevant_product_for_supplier( $product, $supplier ) ) {
+                if ( $item->get_variation_id() !== 0 ) {
+                    $new_id = $item->get_variation_id();
+                } else {
+                    $new_id = $product->get_id();
+                }
+            
+                $new_quantity_before_refund = $item->get_quantity();
+                $quantity_refunded = $order->get_qty_refunded_for_item( $item_id );
+                $new_quantity = $new_quantity_before_refund + $quantity_refunded;
         
-                if ( pq_is_relevant_product_for_supplier( $product, $supplier ) ) {
-                    if ( $item->get_variation_id() !== 0 ) {
-                        $new_id = $item->get_variation_id();
-                    } else {
-                        $new_id = $product->get_id();
+                $is_new_id = true;
+        
+                foreach ( $products as $id => $quantity ) {
+                    if ( $new_id == $id ) {
+                        $products[ $id ] += $new_quantity;
+                        $is_new_id = false;
                     }
-                
-                    $new_quantity_before_refund = $item->get_quantity();
-                    $quantity_refunded = $order->get_qty_refunded_for_item( $item_id );
-                    $new_quantity = $new_quantity_before_refund + $quantity_refunded;
-            
-                    $is_new_id = true;
-            
-                    foreach ( $products as $id => $quantity ) {
-                        if ( $new_id == $id ) {
-                            $products[ $id ] += $new_quantity;
-                            $is_new_id = false;
-                        }
-                    }
-            
-                    if ( $is_new_id ) {
-                        $new_product = array( $new_id => $new_quantity );
-                        $products += $new_product;
-                    }
+                }
+        
+                if ( $is_new_id ) {
+                    $new_product = array( $new_id => $new_quantity );
+                    $products += $new_product;
                 }
             }
         }
-
-        ob_start();
-
-        ?>
-        <table>
-            <tr>
-                <th>Produit</th>
-                <th>Quantité</th>
-                <th>Poids</th>
-            </tr>
-
-        <?php
-
-        foreach ( $products as $product_id => $quantity ) {
-            $product = wc_get_product( $product_id );
-            $short_name = get_post_meta( $product_id, '_short_name', true);
-            $weight = get_post_meta( $product_id, '_pq_weight', true );
-            $unit = get_post_meta( $product_id, '_lot_unit', true );
-            $weight_with_unit = $weight . $unit;
-            ?>
-            <tr>
-                <td><?php echo $short_name; ?></td>
-                <td><?php echo $quantity; ?></td>
-                <td><?php echo $weight_with_unit; ?></td>
-            </tr>
-            <?php
-        }
-        ?>
-        </table>
-        <?php
-
-        return ob_get_clean();
     }
+
+    return $products;
 }
 
 
@@ -92,15 +112,15 @@ function pq_supplier_order_fct() {
  */
 function pq_is_relevant_product_for_supplier( $product, $supplier_to_count ) {
     $product_id = $product->get_id();
-    $supplier_ids = get_terms(array( 
-        'taxonomy' => 'pq_producer',
+    $product_suppliers = get_terms(array( 
+        'taxonomy' => array('product_tag', 'pq_producer'),
         'object_ids' => $product_id,
     ));
 
     $is_product_to_count = false;
   
-    foreach ( $supplier_ids as $supplier_id ) {
-        if ( $supplier_id == $supplier_to_count ) {
+    foreach ( $product_suppliers as $product_supplier ) {
+        if ( $product_supplier->term_id == $supplier_to_count->term_id ) {
             $is_product_to_count = true;
         }
     }
