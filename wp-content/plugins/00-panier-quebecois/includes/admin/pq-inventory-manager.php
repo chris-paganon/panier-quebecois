23a8d03bb4683818838de2358fb8fa5633e105a9
PixelJC <cpaganon@gmail.com>
2022-08-11 12:22:41 -0400

    Display inventory manager from a template

--- wp-content/plugins/00-panier-quebecois/includes/admin/pq-inventory-manager.php
+++ wp-content/plugins/00-panier-quebecois/includes/admin/pq-inventory-manager.php
@@ -10,10 +10,10 @@ if ( !defined( 'ABSPATH' ) ) {
 add_shortcode( 'pq_inventory_manager', 'pq_inventory_manager_fct' );
 
 function pq_inventory_manager_fct() {
-
-  if ( current_user_can( 'pq_see_operations' ) ) {
-    echo 'SUCCESS';
-  } else {
-    echo 'FAILURE';
-  }
+	if ( current_user_can( 'pq_see_operations' ) ) {
+		$args = array();
+		ob_start();
+		wc_pq_get_template( 'pq-inventory-manager-table.php', $args );
+		return ob_get_clean();
+	}
 }
