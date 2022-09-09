71fb377244a4e2404d7a553ce434f06e40d05674
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-09 10:35:18 -0400

    Revert to div wrappers for form fields

--- wp-content/plugins/00-panier-quebecois/templates/admin/pq-missing-products-manager-content.php
+++ wp-content/plugins/00-panier-quebecois/templates/admin/pq-missing-products-manager-content.php
@@ -8,28 +8,28 @@ if ( !defined( 'ABSPATH' ) ) {
 
 <form id="missing-product-form">
 
-  <p class="pq-form-row missing-product-type-wrapper">
+  <div class="pq-form-row missing-product-type-wrapper">
     <label for="missing-product-type">Type de produit manquant:</label>
     <select name="missing-product-type" id="missing-product-type">
       <option value="replacement">Remplacement</option>
       <option value="organic-replacement">Remplacement BIO</option>
       <option value="refund">Remboursement</option>
     </select>
-  </p>
+  </div>
 
-  <p class="pq-form-row product-selection-wrapper" id="missing-product-wrapper">
+  <div class="pq-form-row product-selection-wrapper" id="missing-product-wrapper">
     <label for="pq-missing-short-name-search-box">Produit manquant:</label>
     <input type="text" id="pq-missing-short-name-search-box" name="pq-missing-short-name-search-box" class="pq-short-name-search-box">
     <input type="hidden" id="selected-missing-product" name="selected-missing-product" class="selected-product">
     <ul class="pq-search-results"></ul>
-	</p>
+  </div>
 
-  <p class="pq-form-row product-selection-wrapper" id="replacement-product-wrapper">
+  <div class="pq-form-row product-selection-wrapper" id="replacement-product-wrapper">
     <label for="pq-replacement-short-name-search-box">Produit de remplacement:</label>
     <input type="text" id="pq-replacement-short-name-search-box" name="pq-replacement-short-name-search-box" class="pq-short-name-search-box">
     <input type="hidden" id="selected-replacement-product" name="selected-replacement-product" class="selected-product">
     <ul class="pq-search-results"></ul>
-  </p>
+  </div>
 
   <button id="review-missing-product">Suivant</button>
 
