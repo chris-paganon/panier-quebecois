3c10077fcfe9400276444b21c4ce992e2293e266
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-12 11:50:21 -0400

    Disable seller of the week if plugin is disabled

--- wp-content/themes/astra-child/woocommerce/myaccount/dashboard/seller-of-the-week.php
+++ wp-content/themes/astra-child/woocommerce/myaccount/dashboard/seller-of-the-week.php
@@ -1,5 +1,5 @@
 <div class="mto-dashboard-container dashboard-seller-week">
-    <?php if ( do_shortcode('[pq_seller_week]') ) : ?>
+    <?php if ( shortcode_exists('pq_seller_week') && do_shortcode('[pq_seller_week]') ) : ?>
         <p class="dashboard-sw-title"><?php echo __('Discover our seller of the week', 'panierquebecois'); ?></p>
         <?php do_shortcode('[pq_seller_week]'); ?>
     <?php endif ?>
