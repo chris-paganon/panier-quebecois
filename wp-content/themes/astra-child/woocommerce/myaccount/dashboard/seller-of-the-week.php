ad4480e54202fca52b1fe05620e2366cfc90955b
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-12 11:14:45 -0400

    Don't display seller of the week content if empty

--- wp-content/themes/astra-child/woocommerce/myaccount/dashboard/seller-of-the-week.php
+++ wp-content/themes/astra-child/woocommerce/myaccount/dashboard/seller-of-the-week.php
@@ -1,4 +1,6 @@
 <div class="mto-dashboard-container dashboard-seller-week">
-    <p class="dashboard-sw-title"><?php echo __('Discover our seller of the week', 'panierquebecois'); ?></p>
-	<?php do_shortcode('[pq_seller_week]'); ?>
+    <?php if ( do_shortcode('[pq_seller_week]') ) : ?>
+        <p class="dashboard-sw-title"><?php echo __('Discover our seller of the week', 'panierquebecois'); ?></p>
+        <?php do_shortcode('[pq_seller_week]'); ?>
+    <?php endif ?>
 </div>
