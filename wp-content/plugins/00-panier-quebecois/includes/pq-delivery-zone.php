97452aeaf16ea39571168b24222b18803fb656d1
chrispaganon <info@chrispaganon.com>
2023-02-12 14:14:43 -0500

    Add cookie & postal code in cart on popup submit

--- wp-content/plugins/00-panier-quebecois/includes/pq-delivery-zone.php
+++ wp-content/plugins/00-panier-quebecois/includes/pq-delivery-zone.php
@@ -3,12 +3,23 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 
 /**
+ * Early enable customer WC_Session in case it is not enabled yet
+ */ 
+add_action( 'init', 'wc_session_enabler' );
+function wc_session_enabler() {
+    if ( ! is_admin() && ! WC()->session->has_session() ) {
+        WC()->session->set_customer_session_cookie( true );
+    }
+}
+
+/**
  * Add a popup to select the delivery zone
  */
 add_action( 'wp_footer', 'pq_delivery_zone_popup' );
 
 function pq_delivery_zone_popup() {
   if ( !isset( $_COOKIE['pq_delivery_zone'] ) || $_COOKIE['pq_delivery_zone'] == '0' ) {
+    // todo: do not show if customer is logged in & has a postal code set
     $args = array();
     wc_pq_get_template( 'popup/delivery-zone-select.php', $args );
   }
@@ -25,8 +36,10 @@ function pq_get_delivery_zone_with_ajax() {
 	if ( !isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'pq-delivery-zone-nonce')) wp_die();
 
   $postal_code = sanitize_text_field( $_POST['postal_code'] );
-  $all_zones = WC_Shipping_Zones::get_zones();
-  // $delivery_zone = pq_get_delivery_zone( $postal_code );
+  WC()->customer->set_shipping_postcode($postal_code);
+  WC()->customer->set_billing_postcode($postal_code);
+
+  setcookie( 'pq_delivery_zone', preg_replace('/\s+/', '', $postal_code), time() + (86400 * 30), '/' );
 
   echo $postal_code;
   wp_die();
