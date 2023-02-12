78b64cc53b8a5470f87ff8e64ed1289e9202ff85
chrispaganon <info@chrispaganon.com>
2023-02-12 13:01:21 -0500

    Add simple postal code regex validation

--- wp-content/plugins/00-panier-quebecois/assets/js/pq-delivery-zone.js
+++ wp-content/plugins/00-panier-quebecois/assets/js/pq-delivery-zone.js
@@ -1,5 +1,24 @@
 jQuery(document).ready(function ($) {
+
   $('.delivery-zone-select-popup__close').click(function () {
-    $('.delivery-zone-select-popup-wrapper').hide();
+    $('.delivery-zone-select-popup-wrapper').hide()
   });
+
+  $('#pq-postal-code-submit').click(function () {
+    const postalCode = $('#pq-postal-code').val();
+    const postalCodeIsValid = validatePostalCodeString(postalCode)
+    if (!postalCodeIsValid) {
+      $('.pq-postal-code-response').html('Le code postal est invalide')
+      return
+    } else {
+      $('.pq-postal-code-response').html('')
+      // getDeliveryZone()
+      // setDeliveryZoneCookie()
+    }
+  });
+
+  function validatePostalCodeString(postalCode) {
+    const postalCodeRegex = /[ABCEGHJ-NPRSTVXYabceghj-nprstvxy]\d[ABCEGHJ-NPRSTV-Zabceghj-nprstv-z][ -]?\d[ABCEGHJ-NPRSTV-Zabceghj-nprstv-z]\d/
+    return postalCode.match(postalCodeRegex) === null ? false : true
+  }
 });
--- wp-content/plugins/00-panier-quebecois/templates/popup/delivery-zone-select.php
+++ wp-content/plugins/00-panier-quebecois/templates/popup/delivery-zone-select.php
@@ -11,6 +11,8 @@ if ( !defined( 'ABSPATH' ) ) exit;
     <div class="delivery-zone-select-popup__content">
       <h3>Entrez votre code postal</h3>
       <input type="text" name="pq-postal-code" id="pq-postal-code" placeholder="H0H 0H0" />
+      <button id="pq-postal-code-submit">Valider</button>
+      <div class="pq-postal-code-response"></div>
     </div>
   </div>
 </div>
